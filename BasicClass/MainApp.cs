bc473dda8437ecbd92357878c8466f5cf4cfbbd6
Raian <leeranleeran30@gmail.com>
2023-03-28 15:42:04 +0900

    03281542

---
+++ BasicClass/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClass
+{
+    class Cat
+    {
+        public string name;
+        public string color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{name} : 야옹");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat(); //Cat kitty - 클래스 변수 생성해서 공간 확보, new는 클래스 호출 연산자, cat();는 생성자
+                                   //형성된 객체인 kitty는 '인스턴스'라고 함
+                                   //Cat kitty = new Cat();는 '인스턴스화'작업
+            kitty.color = "하얀색";
+            kitty.name = "키티";
+            kitty.Meow();
+            Console.WriteLine($"{kitty.name} : {kitty.color}");
+
+            Cat nero = new Cat();
+            nero.color = "검은색";
+            nero.name = "네로";
+            nero.Meow();
+            Console.WriteLine($"{nero.name} : {nero.color}");
+        }
+    }
+}
---
+++ Constructor/MainApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Cat
+    {
+        public Cat()  //생성자 오버로딩, 매개변수가 없는 생성자
+        {
+            Name = "";
+            Color = "";
+        }
+        public Cat(string _Name, string _Color) //생성자 오버로딩, 매개변수가 있는 생성자
+        {
+            Name = _Name;
+            Color = _Color;
+        }
+
+        ~Cat()   //종료자 클래스 이름 앞에 '~' //프로그램 실행 중에는 종료 시점 모르지만, 프로그램이 종료될 때 발동된다. 
+        {
+            Console.WriteLine($"{Name} : 잘가");
+        }
+
+        public string Name;   //필드, 전역변수, 멤버변수
+        public string Color;  //필드, 전역변수, 멤버변수
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : 야용");
+        }
+
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat("키티", "하얀색");               //객체 생성과 동시에 초기값을 부여
+            kitty.Meow();                                       //클래스의 메소드 발동
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat("네로", "검은색");
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {nero.Color}");
+        }
+    }
+}
