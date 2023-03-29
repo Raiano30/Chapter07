aab5c3e8bb1d846bf14e99ed144ec5a312ab4daf
Raian <leeranleeran30@gmail.com>
2023-03-29 15:49:31 +0900

    2303291549

--- StaticField/MainApp.cs
+++ StaticField/MainApp.cs
@@ -8,18 +8,25 @@ namespace StaticField
 {
     class Global
     {
-        public static int Count = 0;
+        public static int Count = 0;  //전역변수, 멤버변수//static : 인스턴스화 필요없이. Global.count로 불러올 수 있다.
+    }
+    class Global2
+    {
+        public int count = 0; 
     }
     class ClassA
     {
-        public ClassA()
+        public ClassA() //생성자 : 한정자  +  클래스 이름과 같다. 
         {
             Global.Count++;
+
+            Global2 a = new Global2(); //static이 아닐 때 인스턴화 
+            a.count++;  //독립성을 가지므로 다른 클래스 참조시 new로 불러올 수 없다.
         }
     }
     class ClassB
     {
-        public ClassB()
+        public ClassB() //생성자
         {
             Global.Count++;
         }
@@ -29,12 +36,10 @@ namespace StaticField
         static void Main()
         {
             Console.WriteLine($"Global.Count : {Global.Count}");
-
-            new ClassA();
+            new ClassA(); //힙에 객체 생성만 하고 만다. 스택의 공간은 없다. 다시 찾아서 접근할 수 없다.
             new ClassA();
             new ClassB();
             new ClassB();
-
             Console.WriteLine($"Global.Count : {Global.Count}");
         }
     }
---
+++ Thie/MainApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This
+{
+    class Employee //getter setter방식
+    {
+        private string Name;       //private: 외부접근 X, 클래스 내부에서는 접근 O
+        private string Position;
+        //public으로하나 바뀌는 것 같지만 private쓰는 것 보안성때문에 그렇다.
+
+        public void SetName(string Name) //Set : 쓰다 //데이타 베이스 보낼때 암호화
+        {
+            this.Name = Name;            //전역변수의 Name = 매개변수 Name;
+        }
+        public string GetName()          //Get  //데이타베이스 받을때 복호화
+        {
+            return Name;
+        }
+        public void SetPosition(string Position)
+        {
+            this.Position = Position;   //전역변수 = 매개변수
+        }
+        public string GetPosition()
+        {
+            return this.Position;
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetName("Pooh");
+            pooh.SetPosition("Waiter");
+            Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
+
+            Employee tigger = new Employee();
+            tigger.SetName("Tigger");
+            tigger.SetPosition("Cleaner");
+            Console.WriteLine($"{tigger.GetName()} {tigger.GetPosition()}");
+        }
+    }
+}
