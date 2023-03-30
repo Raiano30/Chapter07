0bbb95976ad666cd823522a027e1269ebd4fab58
Raian <leeranleeran30@gmail.com>
2023-03-30 15:43:31 +0900

    2303301543

--- AccessModifier/MainApp.cs
+++ AccessModifier/MainApp.cs
@@ -8,18 +8,17 @@ namespace AccessModifier
 {
     class WaterHeater
     {
-        protected int temperature;
+        protected int temperature; //protected 외부에서는 접근X, 파생클래스(상속자)는 접근 O
 
         public void SetTemperature(int temperature)
         {
-            if(temperature < -5 || temperature > 42)
+            if(temperature < -5 || temperature > 42) // 또는
             {
-                throw new Exception("Out of temperature range");
+                throw new Exception("Out of temperature range"); //throw 던진다. catch부분으로 // new :생성 // Excption 스트링 받는다
             }
-            this.temperature = temperature;
+            this.temperature = temperature; //this. 나의. 
         }
-
-        internal void TurnOnWater()
+        internal void TurnOnWater() //같은 어셈블리 코드에서만 public으로 접근 O
         {
             Console.WriteLine($"Turn on water : {temperature}");
         }
@@ -28,7 +27,7 @@ namespace AccessModifier
     {
         static void Main(string[] args)
         {
-            try
+            try //예외처리 조건 
             {
                 WaterHeater heater = new WaterHeater();
                 heater.SetTemperature(20);
@@ -37,8 +36,8 @@ namespace AccessModifier
                 heater.SetTemperature(-2);
                 heater.TurnOnWater();
 
-                heater.SetTemperature(50);
-                heater.TurnOnWater();
+                heater.SetTemperature(50); //예외처리
+                heater.TurnOnWater();      //실행안됨
 
             }
             catch (Exception e)
---
+++ Inheritance/MainApp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Base
+    {
+        protected string Name; //생성자,  protected 자식 클래스까지 접근 가능
+        public Base(string Name)
+        {
+            this.Name = Name;
+            Console.WriteLine($"{this.Name}.Base()");
+        }
+        ~Base()               //종료자 : 가비지 컬렉터로 삭제되기 전에 발동
+        {
+            Console.WriteLine($"{this.Name}.~Base()");
+        }
+        public void BaseMethod()
+        {
+            Console.WriteLine($"{Name}.BaseMethod");
+        }
+    }
+    class Derived : Base //클래스 상속
+    {
+        public Derived(string Name) : base(Name) //base키워드 : 생성자의 매개변수로 넘어간다 
+        {
+            Console.WriteLine($"{this.Name}.Derived()");
+        }
+        ~Derived()
+        {
+            Console.WriteLine($"{this.Name}.~Derived()");
+        }
+        public void DerivedMethod()
+        {
+            Console.WriteLine($"{Name}.DerivedMethod()");
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Base a = new Base("a");
+            a.BaseMethod();
+
+            Derived b = new Derived("b");
+            b.BaseMethod();     //부모클래스의 메소드 발동
+            b.DerivedMethod();  
+        }
+    }//가비지컬렉터가 힙에있는 데이터 지워나간다. 역순으로 지운다. //자식의 종료자 발동 > 부모의 종료자 발동
+}
